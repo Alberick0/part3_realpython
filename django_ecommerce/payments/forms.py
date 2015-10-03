6109d1e81b4a92fce16d72ef2e2ed020344993e0
jean_guzman <Jean_007@hotmail.com>
2015-10-03 00:04:47 -0600

    pushing latest tests

--- django_ecommerce/payments/forms.py
+++ django_ecommerce/payments/forms.py
@@ -14,7 +14,7 @@ class SigninForm(PaymentForm):
 
 
 class CardForm(PaymentForm):
-    last_4_digits = forms.CharField(required=True, min_length=4,
+    last_4_digitss = forms.CharField(required=True, min_length=4,
                                     max_length=4, widget=forms.HiddenInput())
     stripe_token = forms.CharField(required=True, widget=forms.HiddenInput())
 
