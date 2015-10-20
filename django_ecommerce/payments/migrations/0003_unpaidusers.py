2db23f51f6fee30f79c103860c4727e4f0c299c3
jean_guzman <Jean_007@hotmail.com>
2015-10-20 13:01:47 -0600

    added bootstrap3 and jquery

---
+++ django_ecommerce/payments/migrations/0003_unpaidusers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0002_auto_20151011_2302'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UnPaidUsers',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('email', models.CharField(max_length=100, unique=True)),
+                ('last_notification', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
