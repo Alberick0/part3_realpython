2bfc46ec28784021c96b5da9a5757da0c576f60b
jean_guzman <Jean_007@hotmail.com>
2015-10-21 15:59:19 -0600

    refactored marketing__circle_item tag to stop using hardcoded data. Instead using a model to load

--- django_ecommerce/main/templatetags/main_marketing.py
+++ django_ecommerce/main/templatetags/main_marketing.py
@@ -4,15 +4,8 @@ register = template.Library()
 
 
 # This register the func name as a new tag
-@register.inclusion_tag('main/templatetags/circle_item.html')
+@register.inclusion_tag('main/templatetags/circle_item.html',
+                        takes_context=True)
 # declares the HTML that #  is used
-def marketing__circle_item(img_name='yoda.jpg', heading='yoda',
-                             caption='yoda',
-                       button_link='register', button_title='View details'):
-    return {
-        'img': img_name,
-        'heading': heading,
-        'caption': caption,
-        'button_link': button_link,
-        'button_title': button_title
-    }
+def marketing__circle_item(context):
+    return context
--- django_ecommerce/main/views.py
+++ django_ecommerce/main/views.py
@@ -1,15 +1,57 @@
 from django.shortcuts import render_to_response
 
 from payments.models import User
+from main.models import MarketingItem
+
+
+class MarketingItems(object):
+    def __init__(self, img, heading, caption, button_link='register',
+                 button_title='details'):
+        self.img = img
+        self.heading = heading
+        self.caption = caption
+        self.button_link = button_link
+        self.button_title = button_title
+
+
+market_items = [
+    MarketingItems(
+        img="yoda.jpg",
+        heading="Hone your Jedi Skills",
+        caption="All members have access to our unique"
+                " training and achievements ladders. Progress through the "
+                "levels and show everyone who the top Jedi Master is!",
+        button_title="Sign Up Now"
+    ),
+    MarketingItems(
+        img="clone_army.jpg",
+        heading="Build your Clan",
+        caption="Engage in meaningful conversation, or "
+                "bloodthirsty battle! If it's related to "
+                "Star Wars, in any way, you better believe we do it.",
+        button_title="Sign Up Now"
+    ),
+    MarketingItems(
+        img="leia.jpg",
+        heading="Find Love",
+        caption="Everybody knows Star Wars fans are the "
+                "best mates for Star Wars fans. Find your "
+                "Princess Leia or Han Solo and explore the "
+                "stars together.",
+        button_title="Sign Up Now"
+    ),
+]
 
 
 def index(request):
     uid = request.session.get('user')
+    marketing_items = MarketingItem.objects.all()
 
     if uid is None:
         return render_to_response('main/index.html')
 
     else:
         return render_to_response(
-            'main/user.html', {'user': User.get_by_id(uid)}
+            'main/user.html', {'user': User.get_by_id(uid),
+                               'marketing_items': marketing_items}
         )
