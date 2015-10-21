8f4822c1f9d60c3e8f01a586300648c5468eff1f
jean_guzman <Jean_007@hotmail.com>
2015-10-21 00:28:41 -0600

    added new template tag code and html

---
+++ django_ecommerce/main/templatetags/marketing.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+# This register the func name as a new tag
+@register.inclusion_tag('circle_item.html')  # declares the HTML that is used
+def circle_header_item(img_name='yoda.jpg', heading='yoda', caption='yoda',
+                       button_link='register', button_title='View details'):
+    return {
+        'img': img_name,
+        'heading': heading,
+        'caption': caption,
+        'button_link': button_link,
+        'button_title': button_title
+    }
