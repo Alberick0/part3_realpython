4df4964aa14409a8c12d2c1e812c09b7818dd886
jean_guzman <Jean_007@hotmail.com>
2015-10-22 09:38:52 -0600

    added intial data for marketing model, added marketing model to context view

--- django_ecommerce/main/views.py
+++ django_ecommerce/main/views.py
@@ -48,8 +48,8 @@ def index(request):
     marketing_items = MarketingItem.objects.all()
 
     if uid is None:
-        return render_to_response('main/index.html')
-
+        return render_to_response('main/index.html',
+                                  {'marketing_items': marketing_items})
     else:
         return render_to_response(
             'main/user.html', {'user': User.get_by_id(uid),
