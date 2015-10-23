789856489828251a1bbffadb188c7704cab1e026
jean_guzman <Jean_007@hotmail.com>
2015-10-22 20:45:57 -0600

    created migrations for new field in users model

---
+++ django_ecommerce/payments/migrations/0004_user_rank.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0003_unpaidusers'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='rank',
+            field=models.CharField(default='Padwan', max_length=50),
+        ),
+    ]
