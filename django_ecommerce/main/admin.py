a39f5c4fe99f500dcd5925b36b612871571932b6
jean_guzman <Jean_007@hotmail.com>
2015-10-24 09:00:38 -0600

    added Badge model to django admin

--- django_ecommerce/main/admin.py
+++ django_ecommerce/main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import StatusReport, Announcement, MarketingItem
+from .models import StatusReport, Announcement, MarketingItem, Badge
 
 
 admin.site.register(StatusReport)
 admin.site.register(Announcement)
 admin.site.register(MarketingItem)
+admin.site.register(Badge)
+
