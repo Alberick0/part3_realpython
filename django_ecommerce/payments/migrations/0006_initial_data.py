b27f9e627f032d448d823c8255add78c16913713
jean_guzman <Jean_007@hotmail.com>
2015-11-08 12:57:51 -0600

    added a bit of doc in the file to remember

--- django_ecommerce/payments/migrations/0006_initial_data.py
+++ django_ecommerce/payments/migrations/0006_initial_data.py
@@ -3,6 +3,17 @@ from __future__ import unicode_literals
 from django.db import migrations
 from django.contrib.auth.hashers import make_password
 
+"""
+argument 'apps' is of type django.apps.registry.
+apps provides access to the historical models/migrations.
+This is a model that has the state as defied in the previous migration.
+
+argument 'schema_editor' is for changing the schema, which should not be
+necessary very often when migrating data.
+
+'get_model' gives us that historical model
+"""
+
 
 def create_default_user(apps, schema_editor):  # two parameters needed
     new_user = apps.get_model('payments', 'User')
