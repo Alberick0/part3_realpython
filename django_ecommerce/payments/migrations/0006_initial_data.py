432752ad913e9e52d7ac82bf0961635390df9f5d
jean_guzman <Jean_007@hotmail.com>
2015-11-08 12:35:16 -0600

    added initial data(a default user) using migrations

---
+++ django_ecommerce/payments/migrations/0006_initial_data.py
@@ -0,0 +1,26 @@
+# -*- coding utf-8 -*-
+from __future__ import unicode_literals
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+
+def create_default_user(apps, schema_editor):  # two parameters needed
+    new_user = apps.get_model('payments', 'User')
+    try:
+        vader = new_user.objects.get(email='darth@mec.com')
+        vader.delete()
+    except new_user.DoesNotExist:
+        pass
+
+    u = new_user(name='vader', email='darth@mec.com',
+                 last_4_digits='1234', password=make_password('pass')).save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('payments', '0005_user_badge'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_default_user)  # Data migrations
+    ]
