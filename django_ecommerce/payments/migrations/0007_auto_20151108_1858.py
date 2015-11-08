8ef4540d03a5734036aac1dc7b3a7b1eb9bd4013
jean_guzman <Jean_007@hotmail.com>
2015-11-08 12:59:51 -0600

    tested command to add empty migration file

---
+++ django_ecommerce/payments/migrations/0007_auto_20151108_1858.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models, migrations
+
+"""
+File auto generated my makemigrations
+"""
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('payments', '0006_initial_data'),
+    ]
+
+    operations = [
+    ]
